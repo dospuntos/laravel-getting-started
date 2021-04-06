0818b2a23d40b42b848aa09e8ef42a53e4d97ab7
johan@dospuntos.no <johan@dospuntos.no>
2021-04-05 22:35:07 -0400

    Iterations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,16 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        <h1>Some content</h1>
-        <p>{{ 2 == 3 ? "Hello" : "Does not equal" }}
+        <h1>Control Structures</h1>
+        @if(true)
+        <p>This only displayes if it is true</p>
+        @else
+        <p>This only displays if it is false</p>
+        @endif
+        <hr>
+        @for($i = 0; $i < 5; $i++)
+            <p>{{ $i +1 }}. Iteration </p>
+        @endfor
     </div>
 </div>
 @endsection
