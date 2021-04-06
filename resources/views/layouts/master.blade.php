c5b7c4c46e644e1ff72edbb4eb6647b7fcf022ae
johan@dospuntos.no <johan@dospuntos.no>
2021-04-06 00:01:38 -0400

    Adding stylesheets

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,9 +8,11 @@
     <title>Laravel</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
 </head>
 
-<body class="antialiased">
+<body>
     @include('partials.header')
     <div class="container">
         @yield('content')
